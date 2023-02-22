fca186cfff046558be3edb360f5be968c44cef89
Luna Rodrigues Machado <lunarodrigues1142@gmail.com>
2023-02-22 12:49:40 -0300

    Exercício 9

---
+++ modulo-fundamentos/secao-2-introjs/dia-1-primeirospassos/scripts9.js
@@ -0,0 +1,10 @@
+let numeroVerificado1;
+let numeroVerificado2;
+let numeroVerificado3;
+let numeroImpar = false;
+
+if (numeroVerificado1 % 2 !== 0 || numeroVerificado2 % 2 !== 0 || numeroVerificado3 % 2 !== 0) {
+    numeroImpar = true;
+}
+
+console.log(numeroImpar);
