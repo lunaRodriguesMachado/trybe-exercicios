e0fd58a4b4e90d178df84f2d17251f7d5b9ac60b
Luna Rodrigues Machado <lunarodrigues1142@gmail.com>
2023-02-27 15:40:15 -0300

    Terceira parte do exercício 1

--- modulo-fundamentos/secao-2-introjs/dia-2-arrayloopfor/exercicio1.js
+++ modulo-fundamentos/secao-2-introjs/dia-2-arrayloopfor/exercicio1.js
@@ -1,7 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sumNumbers = 0;
+let meanNumbers = 0;
 for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
     sumNumbers = sumNumbers + numbers[index];
 }
+meanNumbers = sumNumbers / numbers.length;
 console.log('A soma de todos os números no array é: ' + sumNumbers);
+console.log('A média entre os números do array é: ' + meanNumbers);
