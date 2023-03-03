3460162610dec4df0cc69986444a1ba06ec774a2
Luna Rodrigues Machado <lunarodrigues1142@gmail.com>
2023-03-03 20:46:31 -0300

    Parte 2 do exercício 2

--- modulo-fundamentos/secao-2-introjs/dia-5-es6/exercicio2.js
+++ modulo-fundamentos/secao-2-introjs/dia-5-es6/exercicio2.js
@@ -6,3 +6,8 @@ let motor = (ligadoOuDesligado) => {
 console.log(motor('desligado'));
 
 // Parte 2
+let areaCirculo = (raio) => {
+    const pi = 3.14;
+    return raio * pi;
+}
+console.log(areaCirculo(5));
