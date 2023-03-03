09e562f9c52a19d897a23a75f2336c68d50b8837
Luna Rodrigues Machado <lunarodrigues1142@gmail.com>
2023-03-03 20:43:12 -0300

    Parte 1 do exercício 2

---
+++ modulo-fundamentos/secao-2-introjs/dia-5-es6/exercicio2.js
@@ -0,0 +1,8 @@
+// Parte 1
+let motor = (ligadoOuDesligado) => {
+    return (ligadoOuDesligado === 'ligado' ? `Desligando o motor` : `Ligando o motor`)
+}
+
+console.log(motor('desligado'));
+
+// Parte 2
